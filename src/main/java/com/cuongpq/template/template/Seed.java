f658fe2692baf210cf5865fde0a188e6c2d59c9e
QuocCuong <quoc.cuong.yb.dhtb@gmail.com>
2024-09-30 21:34:18 +0700

    Update template

---
+++ src/main/java/com/cuongpq/template/template/Seed.java
@@ -0,0 +1,52 @@
+package com.cuongpq.template.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.cuongpq.template.utils.FileUtil.readTokens;
+
+@Service
+@Slf4j
+public class Seed {
+
+    private static final String KEY = "";
+
+    private final List<String> queryIds;
+
+    Seed() {
+        queryIds = readTokens(KEY);
+    }
+
+    private Map<String, String> getHeaders(String queryId) {
+        return Map.of(
+                "Telegram-Data", queryId,
+                "Origin", "https://cf.seeddao.org",
+                "Referer", "https://cf.seeddao.org"
+        );
+    }
+
+    private void claim(String queryId) {
+        try {
+
+        } catch (Exception e) {
+            log.error("Fail to claim.");
+            log.error(e.getMessage());
+        }
+    }
+
+    @Scheduled(cron = "0 0 0/4 ? * *")
+    @EventListener(ApplicationReadyEvent.class)
+    public void claim() {
+        new Thread(() -> {
+            log.info("================ Start Claim Seed ================");
+            queryIds.forEach(this::claim);
+            log.info("================ End Claim Seed ================");
+        }).start();
+    }
+}
